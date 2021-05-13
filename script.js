873748d398e76563a82a5f6564a6c04e527ba39c
DAVID <davidalvg.da@gmail.com>
2021-05-13 11:51:14 -0400

    primer commit

---
+++ script.js
@@ -0,0 +1,11 @@
+//
+//window.onload = function(){
+  //  $('#onload').fadeOut();
+    //$('body').removeClass('onload');
+//}
+
+//setTimeout(function(){
+  //  loader.style.opacity = 0;
+//}, 2500);
+
+
